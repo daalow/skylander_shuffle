f259e16c178a42bf1a5d0a13e2060fde61b72972
Erran Carey <e@ipwnstuff.com>
2013-03-29 22:54:43 -0500

    Update skylander.rb

--- lib/skylander_shuffle/skylander.rb
+++ lib/skylander_shuffle/skylander.rb
@@ -1,18 +1,18 @@
 module SkylanderShuffle
-  class Skylander < Struct.new(:name, :attack, :defense, :speed, :luck, :special)
+  class Skylander < Struct.new(:name, :attack, :defense, :speed, :luck, :characteristics)
     def is_giant?
       if name =~ /(?:bouncer|crusher|eye-brawl|hot head|ninjini|swarm|thumpback|tree rex)/i
         true
-      elsif special
-        true if special[:giant] || special['giant']
+      elsif characteristics && characteristics.include?('giant')
+        true
       end
     end
 
     def is_lightcore?
       if is_giant?
         true
-      elsif special
-        true if special[:lightcore] || special['lightcore']
+      elsif characteristics && characteristics.include?('lightcore')
+        true
       end
     end
   end
