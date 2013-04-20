06bd3b9d43c13a55573eba33f7a9cc04bc1e5edd
Erran Carey <me@errancarey.com>
2013-04-20 14:27:34 -0500

    Updaet lib/skylander_shuffle.rb

--- lib/skylander_shuffle.rb
+++ lib/skylander_shuffle.rb
@@ -1,15 +1,18 @@
 require 'skylander_shuffle/skylander.rb'
 require 'skylander_shuffle/magic_item.rb'
 require 'json'
+require 'net/http'
 
 module SkylanderShuffle
   class << self
     def skylanders
-      JSON.load(Net::HTTP.get_response(URI.parse('https://raw.github.com/ipwnstuff/skylander_shuffle/master/data/skylanders.json')))['skylanders']rescue { error: "Unable to load JSON from Github." }
+      resp_body = Net::HTTP.get_response(URI.parse('https://raw.github.com/ipwnstuff/skylander_shuffle/master/data/skylanders.json')).body
+      JSON.load(resp_body) rescue { error: "Unable to load JSON from Github." }
     end
 
     def magic_items
-      JSON.load(Net::HTTP.get_response(URI.parse('https://raw.github.com/ipwnstuff/skylander_shuffle/master/data/magic_items.json')))['magic_items'] rescue { error: "Unable to load JSON from Github." }
+      resp_body = Net::HTTP.get_response(URI.parse('https://raw.github.com/ipwnstuff/skylander_shuffle/master/data/magic_items.json')).body
+      JSON.load(resp_body) rescue { error: "Unable to load JSON from Github." }
     end
   end
 end
